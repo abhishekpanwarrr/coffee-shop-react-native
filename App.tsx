e2f99e8615da801df00c5097238e57b181fafae2
Abhishek Panwar <abhishekpanwar1718@gmail.com>
2023-11-07 18:18:55 +0530

    added fonts and images

--- App.tsx
+++ App.tsx
@@ -71,14 +71,12 @@ function App(): JSX.Element {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
+           Abhishek Panwar
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
